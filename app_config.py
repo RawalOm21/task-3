5a38d57193167d1d2671345db021f8800b49a7e9
Om Rawal <omepic123@gmail.com>
2024-12-18 11:26:24 +0530

    adding iat and exp to the token

--- app_config.py
+++ app_config.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 class Config:
     SECRET_KEY = random._urandom(32)
     JWT_SECRET_KEY = random._urandom(32)
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=20)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=5)
+    JWT_INITIAL_TOKEN_EXPIRATION = timedelta(minutes=1)
     SQLALCHEMY_DATABASE_URI = 'books.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
